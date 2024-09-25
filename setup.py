8796b6b763598780ce04072cfdde999c106843ff
Jason (Zixian) Zang <156871964+zixian-zang-ai@users.noreply.github.com>
2024-09-25 14:42:53 -0700

    more

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open("requirements_package.txt", "r") as f:
+with open("requirements_demos.txt", "r") as f:
     requirements = f.read().splitlines()
 
 setup(
