5bd66a670664c63c63089e24293a098414a5e7ed
Jason (Zixian) Zang <156871964+zixian-zang-ai@users.noreply.github.com>
2024-09-26 15:11:50 -0700

    no cuda

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ with open("requirements_demos.txt", "r") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="Resemblyzer",
-    version="0.1.4",
+    name="Resemblyzer-python3.9",
+    version="0.1.2",
     packages=find_packages(),
     package_data={
         "resemblyzer": ["pretrained.pt"]
     },
     python_requires=">=3.5",
     install_requires=requirements,
-    author="Corentin Jemine",
-    author_email="corentin@resemble.ai",
+    # author="Corentin Jemine",
+    # author_email="corentin@resemble.ai",
     description="Analyze and compare voices with deep learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/resemble-ai/Resemblyzer",
+    url="https://github.com/zixian-zang-ai/Resemblyzer-python3.9",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
