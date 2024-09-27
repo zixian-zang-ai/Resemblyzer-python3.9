2b72993376e953d4323b78d2fc5e1319517a7737
Jason (Zixian) Zang <156871964+zixian-zang-ai@users.noreply.github.com>
2024-09-26 23:18:50 -0700

    torch 171

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements_demos.txt", "r") as f:
 
 setup(
     name="Resemblyzer-python3.9",
-    version="2.0.2",
+    version="2.0.3",
     packages=find_packages(),
     package_data={
         "resemblyzer": ["pretrained.pt"]
