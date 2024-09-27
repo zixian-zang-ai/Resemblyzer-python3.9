58dac5996c42c290d1d4f11ee86e1f37f7dbe049
Jason (Zixian) Zang <156871964+zixian-zang-ai@users.noreply.github.com>
2024-09-26 21:07:06 -0700

    reload correct

--- resemblyzer/voice_encoder.py
+++ resemblyzer/voice_encoder.py
@@ -2,15 +2,13 @@ from resemblyzer.hparams import *
 from resemblyzer import audio
 from pathlib import Path
 from typing import Union, List
+import importlib
+import sys
+importlib.reload(sys)
+
 from torch import nn
 from time import perf_counter as timer
 import numpy as np
-import importlib
-import sys
-
-importlib.reload(sys)
-
-import torch
 
 
 class VoiceEncoder(nn.Module):
